1ce4e364cbfe44ef76b59f299a78866c91a0bc37
Romulo-Moraes <romulo.moraes.dev@gmail.com>
2024-12-21 17:37:25 -0300

    Make the keyboard input procedure non-blocking

--- src/input.asm
+++ src/input.asm
@@ -5,19 +5,35 @@
 ; dx - on down handler
 ;
 ; @returns 
-; ax - the selected handler, 0x0 if invalid input
+; ax - the selected handler, 0x0 if invalid or no user input
 keyboard_input:
     ; save registers and allocate memory
     push bp
     mov bp, sp
-    sub sp, 2
+    sub sp, 3
 
     ; save the left handler on stack
-    mov [bp-2], ax
+    mov [bp-3], ax
+    mov BYTE [bp-1], 0x0
+
+last_input_loop:
+
+    ; check if there is something on input buffer
+    mov ah, 0x1
+    int 0x16
+
+    jz uppercase_check
+
+    mov [bp-1], al
 
     ; read char from input buffer
     mov ah, 0x0
     int 0x16
+
+    jmp last_input_loop
+
+uppercase_check:
+    mov al, [bp-1]
 
     ; check if the char is uppercase
     cmp al, 0x61
@@ -30,7 +46,7 @@ is_uppercase:
 
     jne not_left
 
-    mov ax, [bp-2]
+    mov ax, [bp-3]
     jmp input_end
 not_left:
     cmp al, 'W'
@@ -54,7 +70,8 @@ not_right:
     mov ax, dx
     jmp input_end
 not_down:
-    ; return 0x0 if invalid user input
+    ; return 0x0 if invalid
+    ; or no user input
     mov ax, 0x0
 input_end:
     mov sp, bp
