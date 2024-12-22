8c69ce12e8a9a998b5f3a0790a875cd0532f7386
Romulo-Moraes <romulo.moraes.dev@gmail.com>
2024-12-22 18:14:01 -0300

    Implement a module that handles collisons

---
+++ src/collisions.asm
@@ -0,0 +1,26 @@
+; @params
+; al - the character
+check_collision:
+    cmp al, SNAKE_BODY_CHAR
+
+    jne not_body_collision
+
+    jmp $
+    
+    jmp check_collision_end
+not_body_collision:
+    cmp al, FRUIT_CHAR
+
+    jne not_fruit_collision
+
+    mov bx, 80
+    mov cx, 25
+    call generate_fruit
+
+    mov ah, 0x1
+
+    jmp check_collision_end
+not_fruit_collision:
+    mov ah, 0x0
+check_collision_end:
+    ret
